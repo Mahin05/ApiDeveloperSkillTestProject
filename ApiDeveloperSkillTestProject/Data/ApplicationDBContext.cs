89c5c98ce6c86390c839921d0ac9e83a8f937c16
mahin05 <mahin.islam05@gmail.com>
2025-06-16 15:40:40 +0600

    Add project files.

---
+++ ApiDeveloperSkillTestProject/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace PacificKnitDivisionWebPortal.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<ProductModel> products { get; set; }
+
+    }
+}
---
+++ ApiDeveloperSkillTestProject/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Model
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
